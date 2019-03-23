abc46d4493b3f1f9c391e0bbab1924e1a77a8059
slava-ff <slavik-28@mail.ru>
2019-03-24 01:05:51 +0300

    feat: 81 works

--- src/index.js
+++ src/index.js
@@ -4,22 +4,23 @@ module.exports = function getZerosCount(number, base) {
   /////////////////// 1. разложение base на простые множители /////////////////// +++++
   console.log('number: ', number);
   console.log('base: ', base);
+  let baseTwo = base;
   let j = 0;
   let n = 2;
   let mnojiteli = new Array();
   do {
-    if (base % n == 0) {
+    if (baseTwo % n === 0) {
       mnojiteli[j] = n;
       j++;
-      base = base / n;
+      baseTwo = baseTwo / n;
     } else {
       n++;
     }
-  } while (n < base);
+  } while (n < baseTwo);
   mnojiteli[j] = n;
   console.log('mnojiteli: ', mnojiteli);
 
-  /////////////// 2. делим number на каждый уникальный множитель ///////////////////
+  /////////////// 2. считаем одинаковые ///////////////////
   let result = [];
   for (let i=0; i < mnojiteli.length; i++) {
     if (mnojiteli[i] != mnojiteli[i-1]) {
@@ -31,10 +32,17 @@ module.exports = function getZerosCount(number, base) {
         }
         console.log('odinak: ', odinak);
       }
-      
+
+
+
+      /////////////// 3. делим number на каждый уникальный множитель /////////////////// ЗДЕСЬ ОШИБКА
       let count = 0;
-       for (let m = mnojiteli[i]; number/m > 1; m=m*m) {
-        count = count + Math.floor(number/m);
+       for (let m = mnojiteli[i]; Math.floor(number/m) > 1; m=m*mnojiteli[i]) {  // 0 поставил вместо 1
+        count = count + Math.floor(number/m); 
+        // 2 Варианта. либо math.floor отсекает не тогда когда надо, и теряем мелочи
+        // либо забываем поделить еще раз, если делится. Хотя м=м*м разве так не делает?
+        // 100/2 = 50, оно больше 1, каунт = 50, 2*
+        // 
       }
       if (odinak !=0) {count = Math.floor(count/odinak);} // !!!!!!!!!
       
@@ -44,6 +52,7 @@ module.exports = function getZerosCount(number, base) {
   }
   console.log('result: ', result);
   
+  //////// 4. ЗДЕСЬ Я ВЫБИРАЮ НАИМЕНЬШЕЕ ЗНАЧЕНИЕ ИЗ МАССИВА RESULT[......];
   let finalResult = result[0];
   for (i=0; i<result.length; i++) {
     if (i+1 < result.length && result[i+1] < result[i]) {
@@ -55,3 +64,5 @@ module.exports = function getZerosCount(number, base) {
   
 
 }
+
+//getZerosCount(70497638, 2);
